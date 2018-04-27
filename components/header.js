3ab44140529150d70f93cb2738cd6e60029306f5
Marcus Greasly <marcus.greasly@avanade.com>
2018-04-28 01:14:47 +1000

    Updated header

--- components/header.js
+++ components/header.js
@@ -1,10 +1,11 @@
 import { Component } from 'preact';
-import Toolbar from 'preact-material-components/Toolbar';
 import Drawer from 'preact-material-components/Drawer';
 import List from 'preact-material-components/List';
-import 'preact-material-components/Toolbar/style.css';
+import Toolbar from 'preact-material-components/Toolbar';
 import 'preact-material-components/Drawer/style.css';
 import 'preact-material-components/List/style.css';
+import 'preact-material-components/Theme/style.css';
+import 'preact-material-components/Toolbar/style.css';
 
 export default class Header extends Component {
     closeDrawer = () => { this.drawer.MDComponent.open = false; }
@@ -26,8 +27,9 @@ export default class Header extends Component {
                     <Drawer.DrawerHeader>AvaGo Smart Camera</Drawer.DrawerHeader>
 					<Drawer.DrawerContent>
 						<List>
-							<List.LinkItem onClick={this.linkTo('/')}>Camera</List.LinkItem>
-							<List.LinkItem onClick={this.linkTo('/results')}>results</List.LinkItem>
+							<List.LinkItem onClick={this.linkTo('/')}>Home</List.LinkItem>
+							<List.LinkItem onClick={this.linkTo('/camera')}>Camera</List.LinkItem>
+							<List.LinkItem onClick={this.linkTo('/list')}>List</List.LinkItem>
 						</List>
 					</Drawer.DrawerContent>				
                 </Drawer.TemporaryDrawer>
